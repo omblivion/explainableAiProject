929c7324139bee74a608cdbff3ce79c2b6d762b3
Raul Gatto <126099719+Raoolo@users.noreply.github.com>
2024-07-26 19:08:37 +0200

    Solving raul pull problems

--- SentimentAnalyzer.py
+++ SentimentAnalyzer.py
@@ -34,34 +34,40 @@ class SentimentAnalyzer:
         else:
             return None
 
-    # Generate synthetic data using Hugging Face model
-    def generate_synthetic_data(self, topic, text, n_samples):
+    # Generate synthetic data using the FLAN model
+    def generate_synthetic_data(self, topic, text, sentiment, n_samples):
         synthetic_data = []
         for _ in range(n_samples):
-            prompt = f"Generate a tweet related to {topic} that expresses sentiment similar to: '{text}'"
+            prompt = f"Generate a tweet related to {topic} that expresses a {sentiment} sentiment similar to: '{text}' "
             inputs = self.flan_tokenizer(prompt, return_tensors="pt").to(self.device)
             outputs = self.flan_model.generate(inputs.input_ids, max_length=60, num_return_sequences=1)
             generated_text = self.flan_tokenizer.decode(outputs[0], skip_special_tokens=True)
             synthetic_data.append(generated_text)
         return synthetic_data
 
-    def augment_training_data(self, topics, n_samples=100):
+    # Augment the training data with synthetic data
+    def augment_training_data(self, topics, texts, sentiments, n_samples=6):
         augmented_data = {'text': [], 'label': []}
         augmented_data_with_topics = {'text': [], 'label': [], 'topic': []}
-        for topic in topics:
-            synthetic_texts = self.generate_synthetic_data(topic, n_samples)
+
+        for topic, text, sentiment in zip(topics, texts, sentiments):
+            synthetic_texts = self.generate_synthetic_data(topic, text, sentiment, n_samples)
+            sentiment_label = self.map_label_to_target(sentiment)
             augmented_data['text'].extend(synthetic_texts)
-            augmented_data['label'].extend([1] * len(synthetic_texts))  # Defaulting to neutral
+            augmented_data['label'].extend([sentiment_label] * len(synthetic_texts))
             augmented_data_with_topics['text'].extend(synthetic_texts)
-            augmented_data_with_topics['label'].extend([1] * len(synthetic_texts))
+            augmented_data_with_topics['label'].extend([sentiment_label] * len(synthetic_texts))
             augmented_data_with_topics['topic'].extend([topic] * len(synthetic_texts))
 
         augmented_df = pd.DataFrame(augmented_data)
         augmented_df_with_topics = pd.DataFrame(augmented_data_with_topics)
         return augmented_df, augmented_df_with_topics
 
-    def fine_tune_with_augmented_data(self, topics, n_samples=6, epochs=3, batch_size=16, learning_rate=2e-5):
-        augmented_train_data, augmented_train_data_with_topics = self.augment_training_data(topics, n_samples)
+    # Fine-tune the model with augmented data
+    def fine_tune_with_augmented_data(self, topics, texts, sentiments, n_samples=6, epochs=3, batch_size=16,
+                                      learning_rate=2e-5):
+        augmented_train_data, augmented_train_data_with_topics = self.augment_training_data(topics, texts, sentiments,
+                                                                                            n_samples)
         return self.fine_tune(augmented_train_data, epochs, batch_size, learning_rate), augmented_train_data_with_topics
 
     # Fine-tune the model on a custom dataset
