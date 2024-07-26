a1523cd28b4df1b96a7a0717dfe694d1db4c14ef
Leonardo Moraglia <omblivion@gmail.com>
2024-07-26 10:48:50 +0200

    test gdown 2

--- DownloadDatasets.py
+++ DownloadDatasets.py
@@ -9,12 +9,16 @@ def download_datasets():
         "tweets_dataset": "https://drive.google.com/file/d/1uw_FBtp7ak2B6VAyt9R7cKfiTVe2buqG/view?usp=sharing"
     }
 
+    # Get the current working directory
+    base_path = os.getcwd()
+
     # Create the dataset directory if it doesn't exist
-    os.makedirs("datasets", exist_ok=True)
+    dataset_dir = os.path.join(base_path, "datasets")
+    os.makedirs(dataset_dir, exist_ok=True)
 
     # Download the dataset if it doesn't already exist
     for dataset_name, url in URLS.items():
-        csv_filepath = f"datasets/{dataset_name}.csv"
+        csv_filepath = os.path.join(dataset_dir, f"{dataset_name}.csv")
         if not os.path.exists(csv_filepath):
             print(f"Downloading {dataset_name}")
             gdown.download(url, csv_filepath, fuzzy=True)
