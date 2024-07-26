221b2a587ef5b48abafd418d81600e2b19db73c0
Leonardo Moraglia <omblivion@gmail.com>
2024-07-26 10:33:19 +0200

    test gdown 1

--- DownloadDatasets.py
+++ DownloadDatasets.py
@@ -12,8 +12,11 @@ def download_datasets():
     # Create the dataset directory if it doesn't exist
     os.makedirs("datasets", exist_ok=True)
 
-    # Download the dataset
+    # Download the dataset if it doesn't already exist
     for dataset_name, url in URLS.items():
-        print(f"Downloading {dataset_name}")
         csv_filepath = f"datasets/{dataset_name}.csv"
-        gdown.download(url, csv_filepath, fuzzy=True)
+        if not os.path.exists(csv_filepath):
+            print(f"Downloading {dataset_name}")
+            gdown.download(url, csv_filepath, fuzzy=True)
+        else:
+            print(f"{dataset_name} already exists. Skipping download.")
